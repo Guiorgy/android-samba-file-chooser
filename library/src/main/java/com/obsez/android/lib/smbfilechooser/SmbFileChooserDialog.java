26bff9ff7cc100fef5dbb0d4c053f353601e9293
Guiorgy <Guiorgy123@gmail.com>
2019-04-15 12:44:04 +0400

    Update SmbFileChooserDialog.java

--- library/src/main/java/com/obsez/android/lib/smbfilechooser/SmbFileChooserDialog.java
+++ library/src/main/java/com/obsez/android/lib/smbfilechooser/SmbFileChooserDialog.java
@@ -21,7 +21,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.view.Window;
-import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.AdapterView;
 import android.widget.Button;
