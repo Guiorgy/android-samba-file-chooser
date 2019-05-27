930a0f7eb05e39300ee6ae929473c0da782927fa
Guiorgy <Guiorgy123@gmail.com>
2019-05-27 11:47:09 +0400

    fix leakCanary

--- app/src/main/java/com/obsez/android/lib/smbfilechooser/demo/App.java
+++ app/src/main/java/com/obsez/android/lib/smbfilechooser/demo/App.java
@@ -3,21 +3,7 @@ package com.obsez.android.lib.smbfilechooser.demo;
 import android.app.Application;
 import android.content.Context;
 
-import com.squareup.leakcanary.LeakCanary;
-
 public final class App extends Application {
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        if (LeakCanary.isInAnalyzerProcess(this)) {
-            // This process is dedicated to LeakCanary for heap analysis.
-            // You should not init your app in this process.
-            return;
-        }
-        LeakCanary.install(this);
-        // Normal app init code...
-    }
-
     @Override
     protected void attachBaseContext(Context base) {
         super.attachBaseContext(base);
